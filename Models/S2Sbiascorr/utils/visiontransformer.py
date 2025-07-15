d8956a09d484ac13db35b27217e512ff0519ce0f
“kjmayer” <kjmayer@ucar.edu>
2025-07-15 09:39:09 -0600

    changes to ViT

--- Models/S2Sbiascorr/utils/visiontransformer.py
+++ Models/S2Sbiascorr/utils/visiontransformer.py
@@ -45,6 +45,18 @@ def upconv_couplet(in_channels, out_channels, act_fun, *args, **kwargs):
             torch.nn.BatchNorm2d(out_channels),
         )
 
+class UpBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        x = self.upsample(x)
+        x = self.conv(x)
+        
+        return x
+
 
 class AttentionHead(nn.Module):
     def __init__(self, d_model, head_size):
@@ -199,89 +211,102 @@ class VisionTransformer(nn.Module):
         self.max_seq_length = self.n_patches + 1
     
         self.patch_embedding = PatchEmbedding(self.d_model, self.img_size, self.patch_size, self.n_channels)
-        self.positional_encoding = PositionalEncoding( self.d_model, self.max_seq_length)
-        self.transformer_encoder = nn.Sequential(*[TransformerEncoder( self.d_model, self.n_heads) for _ in range(n_layers)])
+        self.positional_encoding = PositionalEncoding(self.d_model, self.max_seq_length)
+        self.transformer_encoder = nn.Sequential(*[TransformerEncoder(self.d_model, self.n_heads) for _ in range(n_layers)])
 
         self.decoder_config = decoder_config
     
         # Decoder
-        self.dense1 = dense(
-            in_features=decoder_config["hiddens_final_in"],
-            out_features=decoder_config["hiddens_final_out"][0],
-            act_fun=decoder_config["hiddens_act_func"][0],
-            reshape_shape=decoder_config["reshape_shape"]
-            )
+        self.upsamp1 = UpBlock(
+            in_channels=decoder_config["filters"][0],
+            out_channels=decoder_config["filters"][1])
+
+        self.upsamp2 = UpBlock(
+            in_channels=decoder_config["filters"][1],
+            out_channels=decoder_config["filters"][2])
+    
+        # self.dense1 = dense(
+        #     in_features=decoder_config["hiddens_final_in"],
+        #     out_features=decoder_config["hiddens_final_out"][0],
+        #     act_fun=decoder_config["hiddens_act_func"][0],
+        #     reshape_shape=decoder_config["reshape_shape"]
+        #     )
 
         
-        self.upconv1 = upconv_couplet(
-            in_channels=decoder_config["filters"][0],
-            out_channels=decoder_config["filters"][1],
-            kernel_size=decoder_config["kernel_size"][0],
-            act_fun=decoder_config["up_act_func"][1],
-            padding=decoder_config["padding"][0],
-            output_padding=decoder_config["output_padding"][0],
-            stride=decoder_config["stride"][0]
-        )
+        # self.upconv1 = upconv_couplet(
+        #     in_channels=decoder_config["filters"][0],
+        #     out_channels=decoder_config["filters"][1],
+        #     kernel_size=decoder_config["kernel_size"][0],
+        #     act_fun=decoder_config["up_act_func"][1],
+        #     padding=decoder_config["padding"][0],
+        #     output_padding=decoder_config["output_padding"][0],
+        #     stride=decoder_config["stride"][0]
+        # )
 
-        self.conv1 = conv_couplet(
-            in_channels=decoder_config["filters"][1],
-            out_channels=decoder_config["filters"][1],
-            kernel_size=decoder_config["kernel_size"][0],
-            act_fun=decoder_config["act_func"][0],
-            padding="same",
-            stride=1
-        )
+        # self.conv1 = conv_couplet(
+        #     in_channels=decoder_config["filters"][1],
+        #     out_channels=decoder_config["filters"][1],
+        #     kernel_size=decoder_config["kernel_size"][0],
+        #     act_fun=decoder_config["act_func"][0],
+        #     padding="same",
+        #     stride=1
+        # )
 
-        self.upconv2 = upconv_couplet(
-            in_channels=decoder_config["filters"][1],
-            out_channels=decoder_config["filters"][2],
-            kernel_size=decoder_config["kernel_size"][1],
-            act_fun=decoder_config["up_act_func"][1],
-            padding=decoder_config["padding"][1],
-            output_padding=decoder_config["output_padding"][1],
-            stride=decoder_config["stride"][1]
-        )
+        # self.upconv2 = upconv_couplet(
+        #     in_channels=decoder_config["filters"][1],
+        #     out_channels=decoder_config["filters"][2],
+        #     kernel_size=decoder_config["kernel_size"][1],
+        #     act_fun=decoder_config["up_act_func"][1],
+        #     padding=decoder_config["padding"][1],
+        #     output_padding=decoder_config["output_padding"][1],
+        #     stride=decoder_config["stride"][1]
+        # )
 
-        self.conv2 = conv_couplet(
-                in_channels=decoder_config["filters"][2],
-                out_channels=decoder_config["filters"][2],
-                kernel_size=decoder_config["kernel_size"][-1],
-                act_fun=decoder_config["act_func"][-1],
-                padding="same",
-                stride=1
-            )
+        # self.conv2 = conv_couplet(
+        #         in_channels=decoder_config["filters"][2],
+        #         out_channels=decoder_config["filters"][2],
+        #         kernel_size=decoder_config["kernel_size"][-1],
+        #         act_fun=decoder_config["act_func"][-1],
+        #         padding="same",
+        #         stride=1
+        #     )
 
     def forward(self, images):
         x = self.patch_embedding(images)
-        # print(x.shape)
         x = self.positional_encoding(x)
-        # print(x.shape)
         x = self.transformer_encoder(x)
         # print(x.shape)
         
-        # B, N, D = x[:, 1:, :].shape  # skip cls token
-        # H = int(self.img_size[0]/self.patch_size[0])
-        # W = int(self.img_size[1]/self.patch_size[1])
-        # x = x[:, 1:, :].permute(0, 2, 1)  # [B, D, N]
-        # x = x.view(B, D, H, W)
+        B, N, D = x[:, 1:, :].shape # skip cls token
+        H = int(self.img_size[0]/self.patch_size[0])
+        W = int(self.img_size[1]/self.patch_size[1])
+        
+        x = x[:, 1:, :].permute(0, 2, 1)  # [B, D, N]
+        x = x.view(B, D, H, W)
         # print(x.shape)
 
-        # 16(batch)x64(D_MODEL)
-        x = x[:,0] # grab cls token (or do average pooling over all the patches)
+        ## creating image from cls token - not spatial
+        ## 16(batch)x64(D_MODEL)
+        # x = x[:,0] # grab cls token (or do average pooling over all the patches)
         # print(x.shape)
         # add dense layer 18x36 = 648
-        x = self.dense1(x)  
+        # x = self.dense1(x)  
         # 16, 1, 18, 36
         # print(x.shape)
 
         # Convolve up to 180x360
-        x = self.upconv1(x)
-        x = self.conv1(x)
-        x = self.upconv2(x)
-        out = self.conv2(x)
+        x = self.upsamp1(x)
+        # print(x.shape)
+        out = self.upsamp2(x)
+        # print(out.shape)
+        # x = self.upconv1(x)
+        # x = self.conv1(x)
+        # print(x.shape)
+        # x = self.upconv2(x)
+        # out = self.conv2(x)
         # print(x.shape)
 
         mu = out[:, 0]
-        sigma = F.softplus(out[:, 1]) + 1e-3
+        sigma = F.softplus(out[:, 1]) + 1e-6
         
         return mu, sigma
